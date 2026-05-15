a8dfde2cc12a8457f810f194baa3409ab4f6aae1
zztwi <zztwitiktok0@gmail.com>
2026-05-15 12:06:36 +0200

    fix resend mail handler

--- src/mail.js
+++ src/mail.js
@@ -3,10 +3,16 @@ import { Resend } from 'resend'
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendEmail({ to, subject, html }) {
-  return resend.emails.send({
+  const result = await resend.emails.send({
     from: process.env.EMAIL_FROM || 'onboarding@resend.dev',
     to,
     subject,
     html,
   })
+
+  if (result.error) {
+    throw new Error(result.error.message)
+  }
+
+  return result.data
 }
