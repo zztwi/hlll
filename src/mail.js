bae2f2d6ab1388327ce7ddc3793497ef323bd0f6
zztwi <zztwitiktok0@gmail.com>
2026-05-15 11:43:01 +0200

    switch mail system to resend

--- src/mail.js
+++ src/mail.js
@@ -19,7 +19,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 
 export async function sendEmail({ to, subject, html }) {
   return resend.emails.send({
-    from: process.env.EMAIL_FROM,
+    from: process.env.EMAIL_FROM || 'onboarding@resend.dev',
     to,
     subject,
     html,
